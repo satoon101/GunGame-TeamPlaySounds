256ddb1e620025578547b19820e91dde6d226338
satoon101 <satoon101@gmail.com>
2025-05-09 20:12:07 -0400

    Updates for Ruff.

--- addons/source-python/plugins/gungame/plugins/custom/gg_teamplay_sounds/configuration.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_teamplay_sounds/configuration.py
@@ -11,27 +11,25 @@ from gungame.core.config.manager import GunGameConfigManager
 # Plugin
 from .info import info
 
-
 # =============================================================================
 # >> ALL DECLARATION
 # =============================================================================
 __all__ = (
-    'announce_final_round',
-    'dominating_levels',
-    'join_team',
+    "announce_final_round",
+    "dominating_levels",
+    "join_team",
 )
 
 
 # =============================================================================
 # >> CONFIGURATION
 # =============================================================================
-with GunGameConfigManager(info.name) as _config:
-
-    with _config.cvar('announce_final_round', 1) as announce_final_round:
-        announce_final_round.add_text()
-
-    with _config.cvar('join_team', 1) as join_team:
-        join_team.add_text()
-
-    with _config.cvar('dominating_levels', 4) as dominating_levels:
-        dominating_levels.add_text()
+with (
+    GunGameConfigManager(info.name) as _config,
+    _config.cvar("announce_final_round", 1) as announce_final_round,
+    _config.cvar("join_team", 1) as join_team,
+    _config.cvar("dominating_levels", 4) as dominating_levels,
+):
+    announce_final_round.add_text()
+    join_team.add_text()
+    dominating_levels.add_text()
--- addons/source-python/plugins/gungame/plugins/custom/gg_teamplay_sounds/gg_teamplay_sounds.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_teamplay_sounds/gg_teamplay_sounds.py
@@ -22,72 +22,73 @@ from gungame.core.weapons.manager import weapon_order_manager
 # Plugin
 from .configuration import announce_final_round, dominating_levels, join_team
 
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+GAME_START_REASON = 15
+
 
 # =============================================================================
 # >> GAME EVENTS
 # =============================================================================
-@Event('round_start')
+@Event("round_start")
 def _round_start(game_event):
     """Play the "final round" sound, if it's the final round."""
-    if 'gg_teamplay' not in gg_plugin_manager:
-        return
-
-    if not announce_final_round.get_bool():
-        return
-
     levels = set(team_levels.values())
-    if len(levels) != 1:
-        return
+    if (
+        "gg_teamplay" not in gg_plugin_manager or
+        not announce_final_round.get_bool() or
+        len(levels) != 1 or
+        list(levels)[0] != weapon_order_manager.max_levels
+    ):
+        return EventAction.CONTINUE
 
-    max_level = weapon_order_manager.max_levels
-    if list(levels)[0] != max_level:
-        return
-
-    # pylint: disable=import-outside-toplevel
     from gungame.plugins.included.gg_teamplay.manager import teamplay_manager
-    if teamplay_manager.current_module == 'deathmatch':
-        return
+    if teamplay_manager.current_module == "deathmatch":
+        return EventAction.CONTINUE
 
-    # pylint: disable=import-outside-toplevel
     from gungame.plugins.included.gg_teamplay.manager import team_dictionary
     instances = team_dictionary.values()
     current_multi_kills = {team.multi_kill for team in instances}
     if len(current_multi_kills) != 1:
-        return
+        return EventAction.CONTINUE
 
     if list(current_multi_kills)[0] + 1 == list(instances)[0].level_multi_kill:
         Delay(
             delay=1,
             callback=sound_manager.play_sound,
-            args=('teamplay_final_round',)
+            args=("teamplay_final_round",),
         )
+        return EventAction.STOP_BROADCAST
+
+    return EventAction.CONTINUE
 
 
-@Event('player_team')
+@Event("player_team")
 def _join_team(game_event):
     """Play the appropriate join team sound."""
     if not join_team.get_bool():
         return
 
-    team = game_event['team']
+    team = game_event["team"]
     if team not in team_levels:
         return
 
-    player = player_dictionary[game_event['userid']]
+    player = player_dictionary[game_event["userid"]]
     Delay(
         delay=1,
         callback=player.play_gg_sound,
-        args=(f'teamplay_join_team_{teams_by_number[team]}',)
+        args=(f"teamplay_join_team_{teams_by_number[team]}",),
     )
 
 
 # =============================================================================
 # >> GUNGAME EVENTS
 # =============================================================================
-@Event('gg_team_level_up')
+@Event("gg_team_level_up")
 def _team_level_up(game_event):
     """Play the appropriate team level-up sound."""
-    scoring_team = game_event['team']
+    scoring_team = game_event["team"]
     if scoring_team not in team_levels:
         return
 
@@ -95,26 +96,26 @@ def _team_level_up(game_event):
     opposing_level = team_levels[5 - scoring_team]
     suffix = teams_by_number[scoring_team]
     if current_level < opposing_level:
-        sound = 'levelup'
+        sound = "levelup"
     elif current_level == opposing_level:
-        sound = 'ties'
+        sound = "ties"
     elif current_level == opposing_level + 1:
-        sound = 'takes_lead'
+        sound = "takes_lead"
     elif current_level < opposing_level + dominating_levels.get_int():
-        sound = 'increases_lead'
+        sound = "increases_lead"
     else:
-        sound = 'dominating'
+        sound = "dominating"
     Delay(
         delay=1,
         callback=sound_manager.play_sound,
-        args=(f'teamplay_{sound}_{suffix}',)
+        args=(f"teamplay_{sound}_{suffix}",),
     )
 
 
 # =============================================================================
 # >> SOUND HOOKS
 # =============================================================================
-@SoundHook('level_up')
+@SoundHook("level_up")
 def _suppress_level_up_sound(sound_name):
     """Stop level-up sound from playing at the same time as other sounds."""
     return False
@@ -123,10 +124,10 @@ def _suppress_level_up_sound(sound_name):
 # =============================================================================
 # >> PRE-EVENTS
 # =============================================================================
-@PreEvent('round_end')
+@PreEvent("round_end")
 def _stop_broadcast(game_event):
     """Stop round_end from broadcasting to clients causing round end sounds."""
-    if game_event['reason'] != 15:
+    if game_event["reason"] != GAME_START_REASON:
         return EventAction.STOP_BROADCAST
 
     return EventAction.CONTINUE
